0630ad18ded3409b46d95738039869144739923d
Krysthalia <38167520+Krysthalia@users.noreply.github.com>
2019-03-28 19:27:00 +0200

    Recherche avec l'API fonctionne. 30 animé trending

--- Mimashita/components/Content.tsx
+++ Mimashita/components/Content.tsx
@@ -8,46 +8,36 @@ interface Props {
   pageName: string;
 }
 
-/*
-const data = [
-  { title: "A", data: [{"Albator"}] },
-  { title: "F", data: ["Fullmetal Alchemist", "Full moon"] },
-  { title: "N", data: ["Naruto", "Nanatsu no taizai"] },
-  { title: "O", data: ["One piece"] },
-  {
-    title: "S",
-    data: [
-      "Sword Art Online",
-      "Shingeki no Kyojin",
-      "Shaman King",
-      "Steins gate"
-    ]
-  }
-]; */
+interface Anime {
+  title: string;
+}
 
 export default class Content extends React.Component<Props> {
-
   constructor(props) {
     super(props);
     this.updatePlanToWatch = this.updatePlanToWatch.bind(this);
     this.updateWatched = this.updateWatched.bind(this);
   }
 
-  updatePlanToWatch(anime: string){
-    alert("Update plan to watch ! "+anime);
+  updatePlanToWatch(anime: string) {
+    alert("Update plan to watch ! " + anime);
   }
-
-  updateWatched(anime: string){
-    alert("Update plan to watch ! "+anime);
+ 
+  updateWatched(anime: string) {
+    alert("Update watched ! " + anime);
   }
 
   render() {
     return (
       <View style={styles.Content}>
-        {this.props.pageName == "Search" && 
-          <Search addPlanToWatch={this.updatePlanToWatch} addToWatched={this.updateWatched} />}
-        {this.props.pageName == "My list" && 
-          <MyList />}
+        {this.props.pageName == "Search" && (
+          <Search
+            addPlanToWatch={this.updatePlanToWatch}
+            addToWatched={this.updateWatched}
+            animeTrendingList={this.props.animeTrendingList}
+          />
+        )}
+        {this.props.pageName == "My list" && <MyList />}
       </View>
     );
   }
